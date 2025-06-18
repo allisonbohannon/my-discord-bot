da1dd998bf555464afc131ff5d1776a2421015bf
Allison Bohannon <akboh.github@gmail.com>
2025-06-17 20:37:58 -0400

    Add base code for pulling in calendar info

---
+++ src/commands/utility/schedule.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getEvents } = require('../../handlers/utility/scheduleHandler');
+
+const main = async () => {
+	const result = await getEvents()
+		.then(data => {
+			return data;
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+	return result;
+};
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('schedule')
+		.setDescription('Lists out all calendar events for the day'),
+	async execute(interaction) {
+		interaction.reply({ content: await main() });
+	},
+};
