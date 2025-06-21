8a56a977d372e68cce1b880ed74e83db45f791bf
Allison Bohannon <akboh.github@gmail.com>
2025-06-21 12:40:00 -0700

    Add option types to bot

--- src/commands/watchlist/add.js
+++ src/commands/watchlist/add.js
@@ -16,18 +16,22 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('add-to-watchlist')
 		.setDescription('Add a movie or show to your watchlist')
-        .addStringOption(option =>
+		.addStringOption(option =>
 			option.setName('title')
 				.setDescription('Input title')
 				.setRequired(true))
-        .addStringOption(option =>
+		.addStringOption(option =>
 			option.setName('media-type')
 				.setDescription('Movie or show')
-				.setRequired(true)),
+				.setRequired(true)
+				.addChoices(
+					{ name: 'movie', value: 'movie' },
+					{ name: 'show', value: 'show' },
+				)),
 	async execute(interaction) {
 		await interaction.deferReply();
-        const title = interaction.options.getString('title');
-        const type = interaction.options.getString('media-type');
+		const title = interaction.options.getString('title');
+		const type = interaction.options.getString('media-type');
 		interaction.editReply({ content: await main(title, type) });
 	},
 };
