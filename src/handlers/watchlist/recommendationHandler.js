ee05ce46e204da507445cd955d630293f02f0c47
Allison Bohannon <akboh.github@gmail.com>
2025-06-26 15:50:23 -0700

    Randomize results

--- src/handlers/watchlist/recommendationHandler.js
+++ src/handlers/watchlist/recommendationHandler.js
@@ -3,13 +3,17 @@ const logger = require('../../utils/logger');
 
 module.exports = async (mediaType, genre) => {
 	logger.info(`Looking for a ${genre} ${mediaType}`);
-	const resp = await Title.findOne({
-		where: { genre: genre },
+	
+	const resp = await Title.findAndCountAll({
+		where: { media_type: mediaType, genre: genre },
 	});
-	logger.info(resp);
-	if (!resp) {
+	//logger.info(resp);
+
+	if (resp.count === 0) {
 		return ('No matches found!');
-	}
-	const rec = resp.get();
-    return (`Why don't you watch the ${rec.media_type} ${rec.title}?`);
+	};
+
+	const randInt = Math.round(Math.random() * resp.count);
+	const rec = resp.rows[randInt].dataValues;
+	return (`Why don't you watch the ${rec.media_type} ${rec.title}?`);
 };
