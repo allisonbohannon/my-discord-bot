d5c9e72ed072b979716cfc9e9186a9106af4fafd
Allison Bohannon <akboh.github@gmail.com>
2025-07-02 12:04:47 -0700

    Update watchlist search for multi genre

--- src/handlers/watchlist/recommendationHandler.js
+++ src/handlers/watchlist/recommendationHandler.js
@@ -1,19 +1,24 @@
-const Title = require('../../db/models/Title');
+const { Genre, Title } = require('../../db/models');
 const logger = require('../../utils/logger');
 
 module.exports = async (mediaType, genre) => {
 	logger.info(`Looking for a ${genre} ${mediaType}`);
-	
+
 	const resp = await Title.findAndCountAll({
-		where: { media_type: mediaType, genre: genre },
+		where: { media_type: mediaType },
+		include: {
+			model: Genre,
+			where: { genre: genre },
+			through: { attributes: [] },
+		},
+		attributes: ['title', 'media_type'],
 	});
-	//logger.info(resp);
 
 	if (resp.count === 0) {
 		return ('No matches found!');
 	};
 
 	const randInt = Math.round(Math.random() * resp.count);
-	const rec = resp.rows[randInt].dataValues;
+	const rec = resp.rows[randInt];
 	return (`Why don't you watch the ${rec.media_type} ${rec.title}?`);
 };
