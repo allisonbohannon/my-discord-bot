1c081ea72cd778689c5b26120b3bb75cdb4a3291
Allison Bohannon <akboh.github@gmail.com>
2025-07-02 11:50:48 -0700

    Update show all to show genres

--- src/handlers/watchlist/showAllHandler.js
+++ src/handlers/watchlist/showAllHandler.js
@@ -1,16 +1,27 @@
-const Title = require('../../db/models/Title');
+const { TimestampStyles } = require('discord.js');
+const { Genre, Title } = require('../../db/models');
 const logger = require('../../utils/logger');
 
 
 module.exports = async () => {
-	const resp = await Title.findAll({});
+	const resp = await Title.findAll({
+		include: Genre,
+	});
 	if (!resp) {
 		return 'No titles found!';
 	}
-	logger.info(resp);
 	let str = 'You have the following titles on your watchlist:\n';
 	resp.forEach(title => {
-		str += `- ${title.title}, a ${title.media_type}\n`;
+		const genresArr = title.Genres;
+		const arrLength = genresArr.length;
+		const multi = arrLength > 1 ? 's' : '';
+		let genreStr = `with the genre${multi}: `;
+		for (let i = 0; i < arrLength - 1; i++) {
+			logger.info(genresArr[i]);
+			genreStr += `${genresArr[i].dataValues.genre}, `;
+		};
+		genreStr += genresArr[arrLength - 1].dataValues.genre;
+		str += `- ${title.title}, a ${title.media_type} ${genreStr}\n`;
 	});
 	logger.info(str);
 	return str;
